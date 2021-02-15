643147f43d591e706619ec5d9a732263136a17e3
hoshangwardak <hoshangwardak@gmail.com>
2021-02-14 20:18:29 -0500

    Login Page

---
+++ src/test/java/com/pages/login.java
@@ -0,0 +1,30 @@
+package com.pages;
+
+import com.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class login {
+
+    public login() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "user")
+    public WebElement usernameInputBox;
+
+    @FindBy(id = "password")
+    public WebElement passwordInputBox;
+
+    @FindBy(id = "submit-form")
+    public WebElement logInButton;
+
+    @FindBy(xpath = "//p[@class='warning wrongPasswordMsg']")
+    public WebElement errorMessage;
+
+
+
+
+
+}
---
+++ src/test/java/com/utilities/ThreadSleep.java
@@ -0,0 +1,15 @@
+package com.utilities;
+
+public class ThreadSleep {
+
+    public static void sleep(int seconds) {
+        seconds *= 1000;
+
+        try {
+            Thread.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
